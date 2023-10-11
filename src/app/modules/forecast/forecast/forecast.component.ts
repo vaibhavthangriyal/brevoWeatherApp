85ad896cb35b2a268e4a39442b62d41d2378b1a8
virtualtechnoman <vaibhav@flutin.com>
2023-10-12 03:44:54 +0530

    fix

--- src/app/modules/forecast/forecast/forecast.component.ts
+++ src/app/modules/forecast/forecast/forecast.component.ts
@@ -19,56 +19,135 @@ export class ForecastComponent implements OnInit {
   }
 
   allCities: City[] = [];
-  selectedCity: any;
-  pageSize = 20;
   currentPage = 1;
+  currentCondition: any;
+  foreCastData: any = [];
+  foreCastDays: number = 3;
+  pageSize = 50;
+  showLoader: boolean = true;
+  selectedCity: any;
 
-  constructor(
-    private cityService: CityService,
-    private forecastService: ForecastService,
-  ) {}
+  constructor(private cityService: CityService, private forecastService: ForecastService) {}
 
   ngOnInit(): void {
     this.getAllCities();
   }
 
+  enableToolTip() {
+    // const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+    // const tooltipList = [...tooltipTriggerList].map((tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl));
+  }
+
   getAllCities() {
     try {
-      this.cityService
-        .getAllCities(this.currentPage, this.pageSize)
-        .subscribe((res) => {
-          this.allCities = [...this.allCities, ...res]; // Append new data to existing data
-          this.currentPage++;
-          if (!this.selectedCity) {
-            this.selectedCity = this.allCities[0];
-            this.getForCast();
-          }
-        });
+      this.cityService.getAllCities(this.currentPage, this.pageSize).subscribe((res) => {
+        this.allCities = [...this.allCities, ...res]; // Append new data to existing data
+        this.currentPage++;
+        if (!this.selectedCity) {
+          this.selectedCity = this.allCities[0];
+          this.fetchData();
+        }
+      });
     } catch (error) {}
   }
 
-  getForCast() {
+  async getForCast() {
     try {
-      this.forecastService
-        .getForecastTillDate(this.selectedCity.lat, this.selectedCity.lon)
-        .subscribe(
-          (res) => {
-            console.log('RES', res.list);
-            const groupedArray = [];
-            for (let i = 0; i < res.list.length; i += 8) {
-              console.log(i)
-              groupedArray.push(res.list.slice(i, i + 8));
-            }
-            console.log('gro', groupedArray)
-          },
-          (error: HttpErrorResponse) => {
-            console.log('ERROR', error);
-          },
-        );
+      const allForecast = await this.forecastService.getForecastTillDate(this.selectedCity.lat, this.selectedCity.lon).toPromise();
+
+      console.log('all', allForecast);
+      const lastElemet = allForecast.list[allForecast.list.length - 1];
+      const firstDate = new Date().getDate() + 1;
+      const lastDate = new Date(lastElemet.dt * 1000).getDate();
+
+      const newArray = new Array(lastDate - firstDate + 1);
+      for (let index = 0; index < allForecast.list.length; index++) {
+        const element = allForecast.list[index];
+        const currentDate = new Date().getDate();
+        const newDate = new Date(element.dt * 1000).getDate();
+        const diff = newDate - currentDate;
+        if (newArray[diff - 1]) {
+          newArray[diff - 1] = {
+            max: newArray[diff - 1].max + element.main.temp_max,
+            min: newArray[diff - 1].min + element.main.temp_min,
+            date: new Date(element.dt * 1000),
+            weather: element.weather[0].id,
+            className: `wi wi-icon-${element.weather[0].id}`,
+            count: newArray[diff - 1].count + 1,
+            description: element.weather[0].description,
+          };
+        } else {
+          newArray[diff - 1] = {
+            max: element.main.temp_max,
+            min: element.main.temp_min,
+            date: new Date(element.dt * 1000),
+            weather: element.weather[0].id,
+            className: `wi wi-icon-${element.weather[0].id}`,
+            count: 1,
+            description: element.weather[0].description,
+          };
+        }
+
+        // if (diff > 0) {
+        //   switch (diff - 1) {
+        //     case 1:
+        //       break;
+        //     case 2:
+        //       break;
+        //     case 3:
+        //       break;
+        //     default:
+        //       break;
+        //   }
+        // }
+      }
+      return newArray;
     } catch (error) {
-      console.error(``, error);
+      throw error;
     }
   }
 
+  async getCurrentConditions() {
+    try {
+      let res: any = await this.forecastService.getCurrentCondition(this.selectedCity.lat, this.selectedCity.lon).toPromise();
+      res.className = `wi wi-icon-${res.weather[0].id}`;
+      return res;
+    } catch (error) {
+      throw error;
+    }
+  }
 
+  async fetchData() {
+    try {
+      this.showLoader = true;
+      this.currentCondition = await this.getCurrentConditions();
+      this.foreCastData = await this.getForCast();
+      console.log('CURRENT', this.currentCondition);
+      console.log('FORECAST', this.foreCastData);
+      this.showLoader = false;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  // pairAndCalculateAverages(data: any) {
+  //   const groupedData = [];
+  //   for (let i = 0; i < data.length; i += 8) {
+  //     const group = data.slice(i, i + 8);
+
+  //     // Calculate average temp_max and temp_min for the group
+  //     const averageTempMax =
+  //       group.reduce((sum: number, item: any) => sum + item.main.temp_max, 0) /
+  //       group.length;
+  //     const averageTempMin =
+  //       group.reduce((sum: number, item: any) => sum + item.main.temp_min, 0) /
+  //       group.length;
+  //     const averageTemp =
+  //       group.reduce((sum: number, item: any) => sum + item.main.temp, 0) /
+  //       group.length;
+  //     groupedData.push({ averageTemp, averageTempMax, averageTempMin, weather: });
+  //   }
+
+  //   return groupedData;
+  // }
 }
--- src/app/services/forecast.service.ts
+++ src/app/services/forecast.service.ts
@@ -7,14 +7,17 @@ import { environment } from 'src/environments/environment.development';
   providedIn: 'root',
 })
 export class ForecastService {
-  baseUrl = 'https://api.openweathermap.org/data/2.5/forecast/';
+  baseUrl = 'https://api.openweathermap.org/data/2.5/';
   apiKey = environment.openweathermap.api;
   constructor(private http: HttpClient) {}
 
   getForecastTillDate(lat: number, lon: number): Observable<any> {
-    const url = `?lat=${lat}&lon=${lon}&cnt=${24}&units=metric&appid=${
-      this.apiKey
-    }`;
+    const url = `forecast/?lat=${lat}&lon=${lon}&units=metric&appid=${this.apiKey}`;
+    return this.http.get(this.baseUrl + url).pipe((data) => data);
+  }
+
+  getCurrentCondition(lat: number, lon: number) {
+    const url = `weather?lat=${lat}&lon=${lon}&units=metric&appid=${this.apiKey}`;
     return this.http.get(this.baseUrl + url).pipe((data) => data);
   }
 }
